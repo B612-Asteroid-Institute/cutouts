a7a7f4b4307cefc39db2ddc2445660c33e020f56
Joachim Moeyens <moeyensj@gmail.com>
2022-05-04 08:03:56 -0700

    Add preamble, postamble to exposure_id_from_url

--- cutouts/io.py
+++ cutouts/io.py
@@ -11,10 +11,32 @@ from astropy.utils.data import download_file
 
 logger = logging.getLogger(__file__)
 
-def exposure_id_from_url(url):
-    id_start = url.find("siaRef=")
-    id_end = url.find("&extn")
-    exposure_id = url[id_start + 6 : id_end - 1].split(".")[0]
+def exposure_id_from_url(
+        url: str,
+        preamble: str = "siaRef=",
+        postamble: str = ".fits.fz"
+    ):
+    """
+    Attempt to determine the exposure ID from a cutout URL.
+
+    Parameters
+    ----------
+    url : str
+        URL to remote cutout.
+    preamble : str, optional
+        URL component expected directly infront of the exposure ID.
+    postamble : str, optional
+        URL component expected directly after the exposure ID. This
+        is sometimes the file extension.
+
+    Returns
+    -------
+    exposure_id : str
+        Exposure ID read from URL.
+    """
+    id_start = url.find(preamble)
+    id_end = url.find(postamble)
+    exposure_id = url[id_start + len(preamble) : id_end]
     return exposure_id
 
 def construct_url(
